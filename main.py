24dd2e60dec466e2f3f9adb0157fd8160b2f3dff
Albenga96 <albertoferroni.af@gmail.com>
2022-02-02 10:11:37 +0100

    Update

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from utils import are_parallel, are_perpendicular
 
 r1 = Line()
 r2 = Line()
-r1.implicit_form(3, 0, 1)
-r2.from_two_points([3, 2], [3, -2])
+r1.implicit_form(3, 1, -4)
+r2.from_two_points([-1, 3], [2, -2])
 
 print(are_parallel(r1, r2))
 print(are_perpendicular(r1, r2))
